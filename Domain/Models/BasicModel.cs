d58947278a884cd7eaec70ca614ec5e669c55601
Alex Morales <am8850@hotmail.com>
2014-03-01 01:35:44 -0500

    model changes

---
+++ Domain/Models/BasicModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+
+
+    public class Rootobject
+    {
+        public int[] array { get; set; }
+        public bool boolean { get; set; }
+        public object _null { get; set; }
+        public int number { get; set; }
+        public InnerObject _object { get; set; }
+        public string _string { get; set; }
+    }
+
+    public class InnerObject
+    {
+        public string a { get; set; }
+        public string c { get; set; }
+        public string e { get; set; }
+    }
+
+
+    class BasicModel
+    {
+    }
+}
