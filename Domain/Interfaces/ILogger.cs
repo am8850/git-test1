c34e798b2b54ecf04cf97dc2aab8cebfe79c9e95
am8850 <am8850mia@gmail.com>
2014-03-01 01:15:07 -0500

    Update ILogger.cs

--- Domain/Interfaces/ILogger.cs
+++ Domain/Interfaces/ILogger.cs
@@ -4,5 +4,7 @@ namespace Domain.Interfaces
     interface ILogger
     {
         void Log(string message);
+        void Write(string message);
+        void Log(string message, int threadid);
     }
 }
